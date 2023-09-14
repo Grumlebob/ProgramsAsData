0dc8b621f3011c859bd09e8e3d9da5021497b3ef
Jacob Grum <jacg@itu.dk>
2023-09-14 12:27:49 +0200

    HelloLex assignment 2

---
+++ Assignment2/HelloLex/hello.fs
@@ -0,0 +1,48 @@
+# 1 "C:\Programming\FSharp\ProgrammerSomData\ProgramsAsDataCodeE2023\Lectures\Lec02\HelloLex\hello.fsl"
+  // starting
+
+module Hello_fslex
+open FSharp.Text.Lexing
+open System
+
+# 8 "C:\Programming\FSharp\ProgrammerSomData\ProgramsAsDataCodeE2023\Lectures\Lec02\HelloLex\hello.fs"
+let trans : uint16[] array = 
+    [| 
+    (* State 0 *)
+     [| 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 1us; 1us; 1us; 1us; 1us; 1us; 1us; 1us; 1us; 1us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 2us; 65535us; |];
+    (* State 1 *)
+     [| 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; |];
+    (* State 2 *)
+     [| 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; 65535us; |];
+    |] 
+let actions : uint16[] = [|65535us; 0us; 1us; |]
+let _fslex_tables = FSharp.Text.Lexing.UnicodeTables.Create(trans,actions)
+let rec _fslex_dummy () = _fslex_dummy() 
+// Rule Tokenize
+and Tokenize  lexbuf =
+  match _fslex_tables.Interpret(0,lexbuf) with
+  | 0 -> ( 
+# 9 "C:\Programming\FSharp\ProgrammerSomData\ProgramsAsDataCodeE2023\Lectures\Lec02\HelloLex\hello.fsl"
+                                     LexBuffer<char>.LexemeString lexbuf 
+# 27 "C:\Programming\FSharp\ProgrammerSomData\ProgramsAsDataCodeE2023\Lectures\Lec02\HelloLex\hello.fs"
+          )
+  | 1 -> ( 
+# 10 "C:\Programming\FSharp\ProgrammerSomData\ProgramsAsDataCodeE2023\Lectures\Lec02\HelloLex\hello.fsl"
+                                     failwith "Lexer error: illegal symbol" 
+# 32 "C:\Programming\FSharp\ProgrammerSomData\ProgramsAsDataCodeE2023\Lectures\Lec02\HelloLex\hello.fs"
+          )
+  | _ -> failwith "Tokenize"
+
+# 13 "C:\Programming\FSharp\ProgrammerSomData\ProgramsAsDataCodeE2023\Lectures\Lec02\HelloLex\hello.fsl"
+  // ending
+
+[<EntryPoint>]
+let main argv =
+      printfn "Hello World from FsLex!\n\nPlease pass a digit:"
+      let input = Console.ReadLine()
+      let res=Tokenize (LexBuffer<char>.FromString input)
+      printfn "The lexer recognizes %s" res
+      0
+
+
+# 3000000 "C:\Programming\FSharp\ProgrammerSomData\ProgramsAsDataCodeE2023\Lectures\Lec02\HelloLex\hello.fs"
